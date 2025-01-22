4229b1b18b21a7a021fc2242bfb55555f7bb7eee
Abeeb Ayobamidele Balogun <balogunayobami2023@gmail.com>
2025-01-22 12:13:26 +0100

    navbar background color change

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -72,7 +72,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="sticky top-0 xl:top-9 z-50 max-w-[1440px] mx-auto w-full py-3 px-4 bg-[#00000020] md:p-4 rounded-md shadow-lg flex justify-between items-center">
+    <div className="sticky top-0 xl:top-9 z-50 max-w-[1440px] mx-auto w-full py-3 px-4 bg-blue-50 md:p-4 rounded-md shadow-lg flex justify-between items-center">
       <div className="flex items-center gap-1">
         <svg
           viewBox="0 0 29 32"
@@ -83,18 +83,18 @@ const Navbar = () => {
           <g clip-path="url(#clip0_165_4140)">
             <path
               d="M26.2254 30.8728C26.1571 30.668 26.0859 30.4514 26.0107 30.2268C25.1162 27.7795 25.1726 27.8339 24.7006 26.3719C24.6927 26.3224 24.6838 26.2759 24.6759 26.2314C24.187 24.7743 23.627 23.1144 22.9967 21.2537C22.9957 21.2557 22.9937 21.2577 22.9927 21.2596C21.1661 15.9308 19.3613 10.6652 17.5456 5.36794C17.0736 4.00679 16.5838 2.59716 16.0762 1.14005C15.531 1.07872 14.972 1.04212 14.3981 1.02926C7.24209 0.859114 0.944999 6.50653 0.138567 13.6724C-0.678749 21.0084 4.16281 27.8765 11.2564 29.3306C16.1485 30.1685 20.8604 30.6918 25.7752 31.2665C26.168 31.2982 26.0493 31.312 26.3847 31.3536C26.3669 31.3001 26.3501 31.2487 26.3323 31.1933C26.1462 30.7205 26.1917 30.8134 26.2264 30.8728H26.2254Z"
-              fill="black"
+              fill="#8dceee"
             />
             <path
               d="M22.9973 21.2537C23.6276 23.1144 24.1877 24.7743 24.6765 26.2314C24.5894 25.7388 24.5379 25.5073 24.8694 25.1274C28.9679 20.4989 29.9771 15.2234 27.4925 9.57503C25.2702 4.52609 21.3231 1.72563 16.0768 1.14001C16.5844 2.59712 17.0742 4.00675 17.5462 5.36791C23.9047 6.93878 27.3649 15.2877 22.9973 21.2537Z"
-              fill="black"
-              stroke="black"
+              fill="#8dceee"
+              stroke="#8dceee"
               stroke-width="0.193873"
               stroke-miterlimit="10"
             />
             <path
               d="M17.5447 5.36792C19.3604 10.6652 21.1652 15.9307 22.9918 21.2596C22.9928 21.2576 22.9948 21.2557 22.9958 21.2537C21.5551 17.004 19.7413 11.7038 17.5447 5.36792Z"
-              fill="black"
+              fill="#8dceee"
             />
           </g>
           <defs>
@@ -110,7 +110,7 @@ const Navbar = () => {
         </svg>
 
         <div
-          className="text-[#000] font-bold text-xs md:text-sm xl:text-xl cursor-pointer"
+          className="text-[#8dceee] font-bold text-xs md:text-sm xl:text-xl cursor-pointer"
           onClick={() => router.replace('/')}
         >
           <p>Quality</p>
